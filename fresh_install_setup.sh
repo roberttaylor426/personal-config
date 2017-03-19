e5b4e1a4aca99c45d49b3be9997ada01ac22cf01
Robert Taylor <robert.taylor426@gmail.com>
2017-03-19 22:31:01 +0000

    Installing Docker.

--- fresh_install_setup.sh
+++ fresh_install_setup.sh
@@ -17,7 +17,22 @@ oracle-java8-installer \
 nautilus-dropbox \
 git \
 autojump \
-sublime-text -y
+sublime-text \
+apt-transport-https \
+ca-certificates \
+curl \
+software-properties-common -y
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt update
+sudo apt install docker-ce -y
+
+sudo usermod -a -G docker robert
 
 cd ~
 curl -L http://install.ohmyz.sh | sh
