61fc0a3557da0e4c22d47d2f4c7626b9335c11d6
Robert Taylor <robert.taylor426@gmail.com>
2017-03-20 10:38:42 +0000

    Installing openvpn.

--- fresh_install_setup.sh
+++ fresh_install_setup.sh
@@ -21,7 +21,8 @@ sublime-text \
 apt-transport-https \
 ca-certificates \
 curl \
-software-properties-common -y
+software-properties-common \
+openvpn -y
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
